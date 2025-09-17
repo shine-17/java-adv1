8730f206106d43cc7403bce2d93f59b6c1db657a
dohun <shine_17@naver.com>
2025-09-17 16:47:23 +0900

    add section 4,5: Thread 제어, Thread lifecycle(join, interrupt, yield)

---
+++ src/main/java/thread/control/CheckedExceptionMain.java
@@ -0,0 +1,17 @@
+package thread.control;
+
+import static util.ThreadUtils.*;
+
+public class CheckedExceptionMain {
+    public static void main(String[] args) throws Exception {
+        throw new Exception();
+    }
+
+    static class CheckedRunnable implements Runnable {
+
+        @Override
+        public void run() /*throws Exception*/ {
+            //throw new Exception(); // 주석 풀면 예외 발생
+        }
+    }
+}
--- src/main/java/thread/start/HelloRunnable.java
+++ src/main/java/thread/start/HelloRunnable.java
@@ -3,6 +3,6 @@ package thread.start;
 public class HelloRunnable implements Runnable {
     @Override
     public void run() {
-        System.out.println(Thread.currentThread().getName() + ": run()");
+        System.out.println(Thread.currentThread().getName() + ": + run()");
     }
 }
