19e960daf0b60dae4294554983a49d9c576c3757
dohun <shine_17@naver.com>
2025-09-23 21:06:22 +0900

    add section 11: CAS (Compare and Set), CAS Lock

---
+++ src/main/java/thread/cas/spinlock/SpinLockMain.java
@@ -0,0 +1,33 @@
+package thread.cas.spinlock;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class SpinLockMain {
+    public static void main(String[] args) {
+//        SpinLockBad spinLock = new SpinLockBad();
+        SpinLock spinLock = new SpinLock();
+
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+                    spinLock.lock();
+                    // critical section
+                    log("비즈니스 로직 실행");
+                    sleep(1);
+                } finally {
+                    spinLock.unlock();
+                }
+            }
+        };
+
+        Thread thread1 = new Thread(task, "Thread-1");
+        Thread thread2 = new Thread(task, "Thread-2");
+
+        thread1.start();
+        thread2.start();
+
+    }
+}
